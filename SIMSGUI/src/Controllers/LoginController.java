ea6c8d21d889e30d365abc1a49527072ffb4115b
Daniel Dymond <danieldxyzdd@gmail.com>
2020-10-23 08:24:30 +1300

    Updated LoginController
    Added Comments, modularised code from the ActionListener to additional methods.

--- SIMSGUI/src/Controllers/LoginController.java
+++ SIMSGUI/src/Controllers/LoginController.java
@@ -1,11 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * LoginController Class
+ * A Controller which facilitates user interactions with the LoginWindow and the Database
+ * Project 2: SIMS GUI Project
+ * @author Daniel Dymond (Group 1 - ID# 17977610) 2020
  */
 package Controllers;
 
-import Views.ExceptionWindow;
 import Views.LoginExceptionWindow;
 import Views.LoginWindow;
 import Views.SessionWindow;
@@ -13,38 +13,79 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
-/**
- *
- * @author danie
- */
-public class LoginController {
+public class LoginController 
+{
     
     DatabaseAgent dBA;
     LoginWindow loginWindow;
     LoginWindowActionListener loginWindowActionListener;
+    
+    // Default Embedded Derby Server URL
     static String DEFAULT_DATABASE_URL = "jdbc:derby://localhost:1527/SIMSDB";
     
+    /**
+     * Creates a new LoginController and spawns a new LoginWindow
+     */
     public LoginController()
     {
         spawnWindow();
     }
     
-    
     /**
-     * Generates and Displays the SearchStudentWindow
+     * Generates and Displays the LoginWindow
      */
     private void spawnWindow() 
     {
         // Create a new Action Listener
         loginWindowActionListener = new LoginWindowActionListener();
            
-        // Create the Search Student Window
+        // Create the Login Window
         this.loginWindow = new LoginWindow(loginWindowActionListener);
 
         // Add the window to the screen and set visible
         loginWindow.setVisible(true);
     }
     
+    /**
+     * Updates the controls of the window, particularly with the DefaultDatabaseCheckBox
+     */
+    private void updateWindow()
+    {
+        // Set the enabled state of the URL Bar based on the checkbox
+        loginWindow.databaseURLField.setEnabled(!loginWindow.defaultDatabaseCheckBox.isSelected());
+
+        // If disabled, re-set the text to the default URL
+        if (!loginWindow.databaseURLField.isEnabled())
+            loginWindow.databaseURLField.setText(DEFAULT_DATABASE_URL);
+    }
+    
+    /**
+     * Attempt to login to the database - where successful, create a new session window.
+     * Otherwise, show LoginExceptionWindow.
+     */
+    private void login()
+    {
+        try 
+        {
+            // Attempt to connect to the database with the details provided
+            dBA = new Controllers.DatabaseAgent(loginWindow.usernameField.getText(), new String(loginWindow.passwordField.getPassword()), loginWindow.databaseURLField.getText());
+            
+            // If dBA is successful, create a new Session Window
+            new SessionWindow(dBA).setVisible(true);
+            loginWindow.dispose();
+        } 
+        
+        // If an exception is caught, show an error message
+        catch (SQLException sqle) {
+            new LoginExceptionWindow(sqle.getMessage()).setVisible(true);
+        } catch (IllegalArgumentException iie) {
+            new LoginExceptionWindow(iie.getMessage()).setVisible(true);
+        }
+    }
+    
+    /**
+     * ActionListener used for LoginWindow to handle events
+     */
     public class LoginWindowActionListener implements ActionListener {
 
         @Override
@@ -52,33 +93,15 @@ public class LoginController {
             
             // If the Login Button is selected, attempt to login
             if (e.getSource().equals(loginWindow.loginButton))
-            {
-                // Attempt to connect to the database with the details provided
-                try {
-                    dBA = new Controllers.DatabaseAgent(loginWindow.usernameField.getText(), new String(loginWindow.passwordField.getPassword()), loginWindow.databaseURLField.getText());
-                    new SessionWindow(dBA).setVisible(true);
-                    loginWindow.dispose();
-                } catch (SQLException sqle) {
-                    new LoginExceptionWindow(sqle.getMessage()).setVisible(true);
-                } catch (IllegalArgumentException iie) {
-                    new LoginExceptionWindow(iie.getMessage()).setVisible(true);
-                }
-            }
+                login();
             
             // If the Close Button is selected, end the program on Exit Code 0
             else if (e.getSource().equals(loginWindow.closeButton))
                 System.exit(0);
             
-            // If the Default Database CheckBox is toggled, update the state
+            // If the Default Database CheckBox is toggled, update the window
             else if (e.getSource().equals(loginWindow.defaultDatabaseCheckBox))
-            {
-                // Set the enabled state of the URL Bar based on the checkbox
-                loginWindow.databaseURLField.setEnabled(!loginWindow.defaultDatabaseCheckBox.isSelected());
-                
-                // If disabled, re-set the text to the default URL
-                if (!loginWindow.databaseURLField.isEnabled())
-                    loginWindow.databaseURLField.setText(DEFAULT_DATABASE_URL);
-            }
+                updateWindow();
         }
         
     }
