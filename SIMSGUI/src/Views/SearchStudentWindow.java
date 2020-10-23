75dd6ee489d12ef888c7b8629ea07477c15868fb
Daniel Dymond <danieldxyzdd@gmail.com>
2020-10-24 09:09:18 +1300

    GUI Improvments for SearchStudentWindow
    Added Context information, and Tooltips to give more information about entry fields.

--- SIMSGUI/src/Views/SearchStudentWindow.java
+++ SIMSGUI/src/Views/SearchStudentWindow.java
@@ -62,6 +62,8 @@ public class SearchStudentWindow extends javax.swing.JInternalFrame implements O
                 return false;
             }
         };
+        tipText = new javax.swing.JLabel();
+        tipText1 = new javax.swing.JLabel();
 
         setClosable(true);
         setIconifiable(true);
@@ -75,22 +77,24 @@ public class SearchStudentWindow extends javax.swing.JInternalFrame implements O
         searchRadioGroup.add(searchByIDRadio);
         searchByIDRadio.setSelected(true);
         searchByIDRadio.setText("Search by Student ID");
+        searchByIDRadio.setToolTipText("Search for a Student by their 10-digit Student ID.");
 
         searchRadioGroup.add(searchByNameRadio);
         searchByNameRadio.setText("Search by Student Name");
+        searchByNameRadio.setToolTipText("Search for a Student by their First and Last Name. Any Student which have names that contain the First and Last Name components will appear in the search results below.");
 
-        studentIDField.setToolTipText("");
+        studentIDField.setToolTipText("Enter a Student's 10-digit Student ID, which constists only of numbers.");
 
         jLabel1.setText("Student ID:");
 
         jLabel2.setText("First Name:");
 
-        firstNameField.setToolTipText("");
+        firstNameField.setToolTipText("Enter a Student's First Name.");
         firstNameField.setEnabled(false);
 
         jLabel3.setText("Last Name:");
 
-        lastNameField.setToolTipText("");
+        lastNameField.setToolTipText("Enter a Student's Last Name.");
         lastNameField.setEnabled(false);
 
         searchByIDButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Views/img/find-16px.png"))); // NOI18N
@@ -103,16 +107,21 @@ public class SearchStudentWindow extends javax.swing.JInternalFrame implements O
         searchByNameButton.setToolTipText("Search for a Student by First and Last Name");
         searchByNameButton.setEnabled(false);
 
-        selectStudentButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Views/img/right-green-arrow-16px.png"))); // NOI18N
+        selectStudentButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Views/img/document-edit-16px.png"))); // NOI18N
         selectStudentButton.setText("Edit Selected Student");
-        selectStudentButton.setToolTipText("Search for a Student by First and Last Name");
+        selectStudentButton.setToolTipText("Edit the Student selected in the table.");
         selectStudentButton.setEnabled(false);
 
         studentSearchResultsTable.setAutoCreateRowSorter(true);
         studentSearchResultsTable.setModel(tableModel);
-        studentSearchResultsTable.setRowSelectionAllowed(true);
         jScrollPane3.setViewportView(studentSearchResultsTable);
         studentSearchResultsTable.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+
+        tipText.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Views/img/bulb-16px.png"))); // NOI18N
+        tipText.setText("Search for a Student by their 10-digit Student ID");
+
+        tipText1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Views/img/bulb-16px.png"))); // NOI18N
+        tipText1.setText("Leave blank to show all Students.");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -145,12 +154,18 @@ public class SearchStudentWindow extends javax.swing.JInternalFrame implements O
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(firstNameField))))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 21, Short.MAX_VALUE)
+                        .addGap(0, 0, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(selectStudentButton, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(searchByIDButton, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 421, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(searchByNameButton, javax.swing.GroupLayout.Alignment.TRAILING))))
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 421, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(tipText)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(searchByIDButton))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(tipText1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(searchByNameButton)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -167,7 +182,9 @@ public class SearchStudentWindow extends javax.swing.JInternalFrame implements O
                     .addComponent(studentIDField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(searchByIDButton)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(searchByIDButton)
+                    .addComponent(tipText))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -181,12 +198,14 @@ public class SearchStudentWindow extends javax.swing.JInternalFrame implements O
                     .addComponent(lastNameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(searchByNameButton)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(searchByNameButton)
+                    .addComponent(tipText1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(selectStudentButton)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(15, Short.MAX_VALUE))
         );
 
         pack();
@@ -210,6 +229,8 @@ public class SearchStudentWindow extends javax.swing.JInternalFrame implements O
     public javax.swing.JButton selectStudentButton;
     public javax.swing.JTextField studentIDField;
     public javax.swing.JTable studentSearchResultsTable;
+    private javax.swing.JLabel tipText;
+    private javax.swing.JLabel tipText1;
     // End of variables declaration//GEN-END:variables
 
     /**
